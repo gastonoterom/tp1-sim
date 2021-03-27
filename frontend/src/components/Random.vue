ba136bf482de4e3d3254da5663778cbbe53765ae
Gaston <gotero@moodtechnology.com.ar>
2021-03-27 18:44:46 -0300

    metodo de python random usa seed

--- frontend/src/components/Random.vue
+++ frontend/src/components/Random.vue
@@ -6,23 +6,55 @@
       header="Simulación - UTN FRC"
       sub-title="Método nativo de python"
     >
-      <b-form @submit="onSubmit">
-        <b-form-group
-          id="input-group-2"
-          label="Cantidad de números a generar:"
-          label-for="input-2"
-        >
-          <b-form-input
-            id="input-2"
-            placeholder="Ingresar cantidad"
-            required
-            v-model="cantidad"
-            @keypress="isNumber($event)"
-          ></b-form-input>
-        </b-form-group>
+      <b-form @submit="onSubmit" class="form-random">
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              id="input-group-1"
+              label="Cantidad a generar:"
+              label-for="input-1"
+            >
+            </b-form-group>
+          </b-col>
+          <b-col>
+            <b-form-group
+              id="input-group-2"
+              label="Semilla:"
+              label-for="input-2"
+            >
+            </b-form-group>
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <b-form-input
+              id="input-1"
+              placeholder="Ingresar cantidad"
+              required
+              v-model="cantidad"
+              @keypress="isNumber($event)"
+            ></b-form-input>
+          </b-col>
+          <b-col>
+            <b-form-input
+              id="input-2"
+              placeholder="Ingresar semilla"
+              required
+              v-model="semilla"
+              @keypress="isNumber($event)"
+            ></b-form-input>
+          </b-col>
+        </b-form-row>
+        <b-form-row>
+          <b-col>
+            <b-button style="margin-top: 10px" type="submit" variant="primary"
+              >Generar</b-button
+            >
+          </b-col>
+        </b-form-row>
       </b-form>
 
-      <div :key="cantidad" class="table-container">
+      <div v-if="randomHash" :key="randomHash" class="table-container">
         <b-table
           id="random-table"
           :per-page="pageSize"
@@ -32,10 +64,10 @@
           :items="randomDataProvider"
         ></b-table>
       </div>
-      <div class="pagination-container">
+      <div v-if="randomHash" class="pagination-container">
         <b-pagination
           v-model="pageNum"
-          :total-rows="cantidad"
+          :total-rows="cantidadFilas"
           :per-page="pageSize"
           aria-controls="random-table"
           align="center"
@@ -53,8 +85,11 @@ export default {
   data() {
     return {
       cantidad: 0,
+      cantidadFilas: 0,
+      semilla: new Date().getTime(),
       pageNum: 0,
       pageSize: 5,
+      randomHash: null,
     };
   },
   methods: {
@@ -62,7 +97,7 @@ export default {
       if (this.cantidad == "") return;
 
       const promise = clienteAxios.get(
-        `/randomPython?cantidad_muestra=${this.cantidad}&pagina=${ctx.currentPage}&pageSize=${this.pageSize}`
+        `/randomPython?cantidad_muestra=${this.cantidad}&pagina=${ctx.currentPage}&pageSize=${this.pageSize}&seed=${this.semilla}`
       );
       // Must return a promise that resolves to an array of items
       return promise.then((data) => {
@@ -77,6 +112,15 @@ export default {
 
     async onSubmit(event) {
       event.preventDefault();
+      if (this.cantidad > 1000000 || this.cantidad <= 0) {
+        alert(
+          "Error: La cantidad de numeros aleatorios a generar debe estar entre 1 y 1000000!"
+        );
+        return;
+      }
+
+      this.randomHash = new Date().getTime();
+      this.cantidadFilas = this.cantidad;
     },
     isNumber: function (evt) {
       evt = evt ? evt : window.event;
@@ -111,5 +155,9 @@ export default {
 
 .pagination-container {
   width: 100%;
+}
+
+.form-random {
+  margin: 10px;
 }
 </style>
--- routes/randomRoute.py
+++ routes/randomRoute.py
@@ -1,20 +1,20 @@
 from app import app
 from flask import request, Response
-from random import random
+import random
 from math import trunc
 import json
 from app import cache
 
 
 @cache.memoize()
-def random_nums(cantidad_muestra):
+def random_nums(cantidad_muestra, seed):
     # Array donde se van a guardar y enviar los numeros randoms
     random_array = []
     # Cantidad de cifras decimales donde va a ser truncado cada numero aleatorio
     cifras_decimales = 4
-
+    random.seed(seed)
     for i in range(cantidad_muestra):
-        random_num = random()
+        random_num = random.random()
         random_array.append({
             'num': trunc(random_num * 10 ** cifras_decimales) /
             10**cifras_decimales
@@ -28,11 +28,12 @@ def randomPython():
     cantidad_muestra = int(request.args.get("cantidad_muestra"))
     pagina = int(request.args.get("pagina"))
     pageSize = int(request.args.get("pageSize"))
+    seed = int(request.args.get("seed"))
 
     if (cantidad_muestra > 1000000):
         cantidad_muestra = 1000000
 
-    random_array = random_nums(cantidad_muestra)
+    random_array = random_nums(cantidad_muestra, seed)
     random_array_sliced = random_array[(
         (pagina - 1) * pageSize):(pagina * pageSize)]
     print(random_array_sliced)
