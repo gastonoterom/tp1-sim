2555a4c1e377030fa55f3e79790dd6ee0f3d9187
Gaston <gotero@moodtechnology.com.ar>
2021-03-27 15:14:42 -0300

    production

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__, template_folder='frontend/dist')
-CORS(app)
+# CORS(app)
 
 
 import frontend
--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 const axios = require("axios").default;
 
 const clienteAxios = axios.create({
-    baseURL: "http://localhost:5000/",
+  baseURL: "",
 });
 
 export default clienteAxios;
